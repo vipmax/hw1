e1c6a0a525f05e1413b2e506146cc91d1b304fc7
vipmax <djvipmax@gmail.com>
2014-03-23 18:57:24 +0400

    added task7

--- src/com/company/Task7.java
+++ src/com/company/Task7.java
@@ -1,7 +1,44 @@
 package com.company;
 
+import java.util.Hashtable;
+import java.util.Map;
+
 /**
  * Created by max on 23.03.2014.
  */
 public class Task7 {
+
+    public Integer countOfDifferentSymbolInWord(String word) {
+
+        if (word.isEmpty())
+            return 0;
+
+        Map<Character, Integer> map = new Hashtable<Character, Integer>();
+
+        for (int i = 0; i < word.length(); ++i)
+            map.put(word.charAt(i), 1);
+
+        return map.size();
+    }
+
+    public String wordFromDifferentSymbols(String... words){
+
+        for (String word : words) {
+            if(countOfDifferentSymbolInWord(word)==word.length())
+            {
+                return word;
+            }
+        }
+
+        return  null;
+    }
+
+    public static void main(String[] args) {
+        Task7 task7 = new Task7();
+        String[] words = General.inputWords();
+
+        System.out.println("Первое слово, состоящее только из различных символов: ");
+        System.out.println(task7.wordFromDifferentSymbols(words));
+
+    }
 }
